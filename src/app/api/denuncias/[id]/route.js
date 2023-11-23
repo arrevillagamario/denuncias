53585a6df46b64562c8e9a192cb3b1761c0d469a
Jairo Erazo <132089241+JairoVErazo@users.noreply.github.com>
2023-11-23 11:53:24 -0600

    holaaaa
    podes quitar el comentario del GET, para incluir al municipio y al usuario

---
+++ src/app/api/denuncias/[id]/route.js
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+export const dynamic = "force-dynamic";
+
+const prisma = new PrismaClient();
+
+export async function GET(request, { params }) {
+  try {
+    console.log("params: ", params);
+    const id = params.id;
+    if (!id) {
+      return NextResponse.json(
+        { error: "Proporciona un numero de trcking" },
+        { status: 400 }
+      );
+    }
+
+    const denuncia = await prisma.denuncias.findUnique({
+      where: {
+        id: parseInt(id, 10),
+      },
+      //include: {
+      //user: true,
+      //municipio: true,
+      //},
+    });
+
+    console.log("denuncia:", denuncia);
+
+    if (!denuncia) {
+      console.log("No se encontró la denuncia");
+      return NextResponse.json(
+        { error: "No se encontro la denuncia" },
+        { status: 404 }
+      );
+    }
+
+    console.log("Respondiendo con denuncia:", denuncia);
+    return NextResponse.json(denuncia);
+  } catch (error) {
+    console.error("Error al procesar la solicitud GET", error);
+    return NextResponse.json(
+      { error: "Error interno del servidor" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/denuncias/route.js
@@ -0,0 +1,38 @@
+import prisma from "@/libs/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    const { user, municipio, descripcion, direccion, estado } =
+      await request.json();
+
+    console.log("user:", user);
+    console.log("id_municipio:", municipio);
+
+    const newDenuncia = await prisma.denuncias.create({
+      data: {
+        user: { connect: { dui: parseInt(user.dui) } },
+        municipio: { connect: { id: parseInt(municipio) } },
+        fecha: new Date(),
+        descripcion,
+        direccion,
+        estado,
+      },
+    });
+
+    return NextResponse.json({
+      message: "Denuncia realizada con éxito",
+      denuncia: newDenuncia,
+    });
+  } catch (error) {
+    console.error("Error al procesar la denuncia:", error);
+
+    return NextResponse.json(
+      {
+        message: "Hubo un error al procesar la solicitud POST al servidor",
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
